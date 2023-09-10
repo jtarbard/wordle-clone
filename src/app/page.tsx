0c171c13eb690517cc8dd89acf484e8715549e92
sc18jt <sc18jt@leeds.ac.uk>
2023-09-10 19:47:52 +0100

    Initial structure and files

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className="font-mono text-center underline">Wordle Clone</h1>
+    </main>
+  )
+}
